05d0c605515269b549fd5610cdda7acee7e1e465
Waren Gonzaga <Waren Gonzaga>
2017-04-09 03:54:02 +0800

    Add gulp and npm

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+"use-strict";
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    fs = require('fs');
+
+gulp.task('default', function() {
+    gutil.log("Hello World");
+    gutil.log("Developed by Waren Gonzaga");
+});
