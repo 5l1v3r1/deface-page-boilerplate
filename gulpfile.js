616d77e6a19e9ebd1f8194aab4917c45a3991586
Waren Gonzaga <Waren Gonzaga>
2017-04-09 23:14:42 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,46 @@
 "use-strict";
 
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    fs = require('fs');
+var gulp    = require('gulp-help')(require('gulp')),
+    gutil   = require('gulp-util'),
+    sass    = require('gulp-sass'),
+    pug     = require('gulp-pug'),
+    rename = require('gulp-rename'),
+    minify = require('gulp-minifier'),
+    options = require('gulp-options'),
+    jshint = require('gulp-jshint'),
+    fs      = require('fs');
 
-gulp.task('default', function() {
-    gutil.log("Hello World");
+gulp.task('pug', false, function buildHTML() {
+  return gulp.src('./src/pug/index.pug')
+    .pipe(pug({
+        doctype: 'html',
+        pretty: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass', false, function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+});
+
+// Builder Tasks
+
+// Developer's Tasks
+gulp.task('devmode', "Developer's Mode", function () {
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/pug/**/*.pug', ['pug']);
+});
+
+// Boilerplate Information
+gulp.task('info', false, function() {
+    gutil.log('Some Information About Boilerplate');
+});
+
+// Default Task
+gulp.task('default', false, function() {
+    gutil.log("Deface Page Boilerplate");
     gutil.log("Developed by Waren Gonzaga");
+    gutil.log("Created by Hackers for Hackers");
 });
